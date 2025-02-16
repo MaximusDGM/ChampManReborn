6047008a276b2080ad311b2a0323f5759acbbf20
Dane <danegmclaren@gmail.com>
2025-02-16 20:36:52 +0000

    Remove unused configuration files and refactor project setup
    Deleted redundant Dockerfiles, appsettings, and launchSettings files, streamlining the project structure. Adjusted project outputs to libraries and introduced entity classes (Person and Player) in the Domain layer for future functionality.

---
+++ ChampManReborn.Domain/Entities/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ChampManReborn.Domain.Entities;
+
+public class Person
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Age { get; set; }
+
+}
---
+++ ChampManReborn.Domain/Entities/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace ChampManReborn.Domain.Entities;
+
+public class Player : Person
+{
+    public required string Position { get; set; }
+    public int Skill { get; set; }
+    public int Stamina { get; set; }
+}
